cf5a5fbe92b56d5dd453af2268d0125d33dc0ce0
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2021-03-10 13:50:19 -0500

    Fixed type

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -39,7 +39,7 @@ export const Login = props => {
 
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Nushell</h1>
+                    <h1>Nutshell</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
                         <label htmlFor="inputEmail"> Email address </label>
