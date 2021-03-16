f5060c6c3dbff56d3d5973d11ce9b8f19c61a117
John Douglas Price <70034359+JohnDPrice@users.noreply.github.com>
2021-03-16 11:17:54 -0400

    Revert "Gg old tasks modify"

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,7 +6,6 @@ import { EventList } from "./event/EventList"
 import { TaskProvider } from './tasks/TaskProvider'
 import { TaskList } from './tasks/TaskList'
 import { TaskForm } from './tasks/TaskForm'
-import { TaskDetail } from './tasks/TaskDetail1'
 import { MessageProvider } from "./messages/MessageProvider"
 import { MessageForm } from "./messages/MessageForm"
 import { MessageList } from "./messages/MessageList"
@@ -17,28 +16,48 @@ export const ApplicationViews = () => {
     return (
       <>
 
-          <MessageProvider>
+
+
+        <Route
+          exact path="/" render={props => {
+            return null
+            // Remove null and return the component which will show news articles
+          }}
+        />
+
+        <Route
+          exact path="/register" render={props => {
+            return null
+            // Remove null and return the component which will handle user registration
+          }}
+        />
+
+        <Route
+          path="/friends" render={props => {
+            return null
+            // Remove null and return the component which will show list of friends
+          }}
+        />
+
+        <MessageProvider>
      
-            <Route exact path="/messages/">
-              < MessageList />
-            </Route>
+         <Route exact path="/messages/">
+            <MessageList />
+         </Route>
 
-            <Route exact path="/messages/create">
-                <MessageList />
-                <MessageForm />
-            </Route>
+         <Route exact path="/messages/create">
+            <MessageList />
+            <MessageForm />
+         </Route>
 
-            <Route exact path="/messages/edit/:messageId(\d+)">
-                <MessageList />
-                <MessageForm />
-            </Route>
-   
-          </MessageProvider>
+         <Route exact path="/messages/edit/:messageId(\d+)">
+            <MessageList />
+            <MessageForm />
+         </Route>
+       
+        </MessageProvider>
 
         <TaskProvider>
-          <Route exact path="/tasks/detail/:taskId(\d+)">
-              <TaskDetail />
-          </Route>
           <Route exact path="/tasks">
               <TaskList />
           </Route>
@@ -49,6 +68,25 @@ export const ApplicationViews = () => {
               <TaskForm />
           </Route>
         </TaskProvider>
+
+
+
+        <EventProvider>
+            <Route exact path="/events">
+                <EventList />
+            </Route>
+            <Route exact path="/events/create">
+                <EventForm />
+                <EventList />
+            </Route>
+            <Route path="/events/edit/:eventId(\d+)">
+                <EventForm />
+                <EventList />
+            </Route>
+        </EventProvider>
+
+
       </>
-    )
-  }
+    );
+}
+
